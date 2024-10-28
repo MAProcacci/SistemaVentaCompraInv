fccbd48b33049fa5e68be8b8e018058cd4b0983b
Manuel Procacci <maprocaccivalero@gmail.com>
2024-10-28 12:31:45 -0400

    "Algunos ajustes al Modulo 'nav_ruta_vci.py' para que use Tkinter y muestre los mensajes de error en una ventana emergente. Todo esto es para cuando se generen los ejecutables para Windows. "

--- nav_ruta_vci.py
+++ nav_ruta_vci.py
@@ -1,8 +1,18 @@
 # nav_ruta_vci.py
 import os
+import tkinter as tk
+from tkinter import messagebox
 
 # Como compilar y generar un EXE con nuitka:
 # nuitka --windows-console-mode=disable --standalone --onefile --enable-plugin=tk-inter --output-dir=dist --output-filename=Inc_Sis_Venta.exe nav_ruta_vci.py
+
+# Función para mostrar un mensaje de error
+def mostrar_error(mensaje):
+    """Función para mostrar un mensaje de error en una ventana de Tkinter."""
+    root = tk.Tk()
+    root.withdraw()  # Oculta la ventana principal
+    messagebox.showerror("Error", mensaje)
+    root.destroy()  # Destruye la ventana después del mensaje
 
 # Función para navegar por la ruta 'VCI'
 def navegar_ruta_vci():
@@ -14,7 +24,7 @@ def navegar_ruta_vci():
     for unidad in unidades:
         ruta = fr"{unidad}:\VCI"  # Crear la ruta para cada unidad
         if os.path.exists(ruta):
-            print(f"Ruta encontrada: {ruta}")
+            #print(f"Ruta encontrada: {ruta}")
             ruta_vci = ruta
             break
 
@@ -22,16 +32,16 @@ def navegar_ruta_vci():
     if ruta_vci:
         try:
             os.chdir(ruta_vci)
-            print(f"Directorio actual: {os.getcwd()}")
+            #print(f"Directorio actual: {os.getcwd()}")
 
             # Ejecutar el archivo VentaCompraInv.exe
             os.system("VentaCompraInv.exe")
 
         except Exception as e:
-            print(f"Error al intentar cambiar al directorio: {e}")
+            mostrar_error(f"Error al intentar cambiar al directorio: {e}")
 
     else:
-        print("La ruta 'VCI' no fue encontrada en las unidades C, D o E.")
+        mostrar_error("La ruta 'VCI' no fue encontrada en las unidades C, D o E.")
 
 # Ejecutar la función
 navegar_ruta_vci()
